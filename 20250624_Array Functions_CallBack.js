aaa4f06e5d962a890499c6594385689ccf3995ac
Ariel <arieltanami122@gmail.com>
2025-06-24 18:35:14 +0300

    Add ex4

--- 20250624_Array Functions_CallBack.js
+++ 20250624_Array Functions_CallBack.js
@@ -12,4 +12,25 @@ let onlyEvenValues = intArr => intArr.filter(num => num % 2 == 0)
 
 console.log(onlyEvenValues(intArr))
 
+//ex3
+3. Write a function called showFirstAndLast which accepts an array as an argument and returns a new array with only the first and last elements from the function’s argument array. The returned array should only contain elements that are strings. 
 
+let showFirstAndLast = arr => [arr.at(0), ]
+
+console.log(showFirstAndLast(intArr))
+
+//ex4
+function vowelCount(str){
+  const vowelChars = ['a', 'e', 'i', 'o', 'u']
+  const counts = Object.fromEntries(vowelChars.map(vc => [vc, 0]))
+  
+  // More efficient than map + filter
+  // Iterates only once
+  for (const char of str){
+    char.toLowerCase() in counts &&
+    counts[char.toLowerCase()]++
+  }
+  return counts
+}
+
+console.log(vowelCount('ariel oo'))
