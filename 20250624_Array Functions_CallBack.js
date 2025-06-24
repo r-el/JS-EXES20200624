5c577b7e59f3610f1d9285b03bbef57640301ddd
Ariel <arieltanami122@gmail.com>
2025-06-24 12:18:20 +0300

    Add ex2

--- 20250624_Array Functions_CallBack.js
+++ 20250624_Array Functions_CallBack.js
@@ -5,6 +5,11 @@ let doubleValues = intArr => intArr.map(num => num * 2)
 
 console.log(doubleValues(intArr))
 
+//ex2
+let intArr = [1,2,3,4]
 
+let onlyEvenValues = intArr => intArr.filter(num => num % 2 == 0)
+
+console.log(onlyEvenValues(intArr))
 
 
